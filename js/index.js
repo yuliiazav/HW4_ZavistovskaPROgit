65448a3ee339df98d9735dfb7bbaa5bebef2d8f4
Your Nameyuliiazav <didiky1993@gmail.com>
2022-08-11 10:10:45 +0300

    added HW4 files

---
+++ js/index.js
@@ -0,0 +1,6 @@
+let circleRadius = +prompt("What is your circle radius?");
+let circleRadiusSquared = Math.pow(circleRadius, 2);
+const pi = Math.PI;
+let squareCircle = pi * circleRadiusSquared;
+
+console.log(squareCircle);
